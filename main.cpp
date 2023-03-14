a7007dc4fd7018a9327e5516d5c41cfa9bf427c3
CasBlease <78170007+CasBlease@users.noreply.github.com>
2023-03-14 08:53:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,58 @@
 #include <vector>
 using namespace std;
 
+
+class Item{
+  public:
+    Item();
+    Item(string uName, double uCost);
+    void Print();
+  //private:
+    string name;
+    double cost;
+};
+
+//Default constructor
+Item::Item(){
+  name = "pencil";
+  cost = 2.3;
+}
+
+//User constructor
+Item::Item(string uName, double uCost){
+  name = uName;
+  cost = uCost;
+}
+
+//prints out item name and cost
+void Item::Print(){
+  cout << "Item " << name << " Costs " << cost << endl;
+}
+
+double compute_item_cost(Item obj, int n){
+  double compCost = obj.cost * n;
+
+  cout << n << " " << obj.name << " costs " << to_string(compCost) << endl;
+  
+  return compCost;
+}
+
+void display_cart_total(vector<Item> uCart){
+  double total;
+  for(int i=0; i<uCart.size(); i++){
+    total += uCart[i].cost;
+    //for some reason when the following line isn't there cout << total prints "$nan"
+    cout << total << endl;
+  }
+
+  total*=1.1;
+  
+  cout << "Your cart costs : $" 
+    << total << " for " 
+    << uCart.size() 
+    << " items (10% tax included)" << endl;
+}
+
 int main()
 {
     srand(time(NULL));
